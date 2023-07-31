ca927b5ce8da24f1423d7a231381e503309d3617
Yotam Raz <yotam.raz@nano-di.com>
2023-07-31 11:18:23 +0300

    use rss instead of vms

--- pyprof/profile_memory.py
+++ pyprof/profile_memory.py
@@ -5,11 +5,11 @@ class ProfileRAM:
     def __init__(self, pid):
         self.__pid = pid
         self.ram_handler = psutil.Process(self.__pid)
-        self.init_measure = self.ram_handler.memory_info().vms / 1024**2
+        self.init_measure = self.ram_handler.memory_info().rss / 1024**2
 
     def get_usage(self):
         """
 
         :return:
         """
-        return self.ram_handler.memory_info().vms / 1024**2
+        return self.ram_handler.memory_info().rss / 1024**2
